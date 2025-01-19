a58668ce7d3c6c9fa1c10b9e655c150fe058a0ef
Roland Vet <RlndVt@protonmail.com>
2025-01-19 23:08:06 +0100

    Introduce configurable maximum backup target size

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -35,6 +35,29 @@ samba_sysadmin_add () {
 # EXPERIMENTAL HELPERS
 #=================================================
 
+time_machine_activate_max_size () {
+    if [ -z "$max_size" ]; then
+        echo "The variable max_size is either not set or empty."
+        if [ -z "${max_size+x}" ]; then
+            echo "The variable max_size does not exist."
+        else
+            echo "The variable max_size is empty."
+        fi
+    else
+        echo "The variable max_size exists."
+    fi
+
+    local max_size=$1;
+    local time_machine_config_path=$2
+    if [[ ! $max_size =~ ^[0-9]+$ ]] || [ "$var" -le 0 ]; then
+        echo "max_size is either not a number or not greater than 0."
+        return 0
+    else
+        echo "max_size is a number and greater than 0."
+    fi
+    sed -i '/^    # fruit:time machine max size = [0-9]\+G$/s/# //' $time_machine_config_path
+}
+
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
